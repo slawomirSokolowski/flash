021574ed741ca43e32acb87ca46766c199642a2b
josh <josch>
2022-02-02 17:30:00 +0100

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by Slawomir on 02/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
