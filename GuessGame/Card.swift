271a65848070bb732cac0b09799f38737f6134bf
josh <josch>
2022-02-04 09:38:05 +0100

    colors

--- GuessGame/Card.swift
+++ GuessGame/Card.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+struct Card {
+    let prompt: String
+    let answer: String
+
+    static let example = Card(prompt: "Who played the 13th Doctor in Doctor Who?", answer: "Jodie Whittaker")
+}
